9829c0f9e52024b32e4bf0050d4cf37e536f6c7f
youssef benamor <youssefbenamor1234@gmail.com>
2025-01-17 15:22:22 +0100

    txt

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/*module.exports = {
+  theme: {
+    extend: {
+      fontfamily: {
+        LyftProUI: ["LyftProUI", "sans-serif"],
+      },
+      colors: {
+        primary: "#523be4",
+      },
+    },
+  },
+};*/
